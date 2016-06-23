38b6d53a0723969c49dd034b2c0c579d837ac824
ChefDK User <chef@ip-10-169-167-26.ec2.internal>
2016-06-23 08:03:23 +0000

    Update default recipe to include setup recipe in workstation cookbook

--- apache/recipes/server.rb
+++ apache/recipes/server.rb
@@ -2,7 +2,7 @@ package 'httpd' do
 	action :install
 end
 file '/var/www/html/index.html' do
-	content '<h1>Hola mundo</h1><br>------------------'
+	content '<h1>Hola mundo</h1><br>Amartin, Jun16'
 end
 service 'httpd' do
 	action [ :enable, :start]
